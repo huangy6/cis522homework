07ae3e8500b31ca91a487f584ed3b56a629cf1c4
Yidi Huang <huangy897@gmail.com>
2023-03-16 00:57:31 -0400

    style fix

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -3,6 +3,15 @@ import torch
 from torch import nn
 
 
+def _conv_block(in_chn, out_chn):
+    """Convolution block"""
+    return [
+        nn.Conv2d(in_chn, out_chn, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(out_chn),
+        nn.ReLU(),
+    ]
+
+
 class Model(nn.Module):
     """
     basic CNN model
@@ -11,15 +20,10 @@ class Model(nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         """Initialize model"""
         super(Model, self).__init__()
-        conv_block = lambda in_chn, out_chn: [
-            nn.Conv2d(in_chn, out_chn, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_chn),
-            nn.ReLU(),
-        ]
         clf_block = [nn.Flatten(1), nn.Linear(2048, num_classes)]
         self.net = nn.Sequential(
-            *conv_block(num_channels, 16),
-            *conv_block(16, 32),
+            *_conv_block(num_channels, 16),
+            *_conv_block(16, 32),
             nn.MaxPool2d(4),
             *clf_block,
         )

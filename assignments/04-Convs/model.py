ac49d10ae08d50d72019af8f726c1d5376b3fe60
Yidi Huang <huangy897@gmail.com>
2023-03-16 01:47:35 -0400

    back to what (seems to) work

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -19,9 +19,9 @@ class Model(nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         """Initialize model"""
         super(Model, self).__init__()
-        clf_block = [nn.Flatten(1), nn.Linear(1024, num_classes)]
+        clf_block = [nn.Flatten(1), nn.Linear(2048, num_classes)]
         self.net = nn.Sequential(
-            *_conv_block(num_channels, 16),
+            *_conv_block(num_channels, 32),
             nn.MaxPool2d(4),
             *clf_block,
         )
